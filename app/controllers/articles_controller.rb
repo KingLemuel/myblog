7985252a269fa69ed81f41addc9b5f97c52e771a
Lemuel Uhuru <lemueluhuru@gmail.com>
2014-09-29 19:20:14 +0000

    update header with navList

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,13 @@ class ArticlesController < ApplicationController
   
   def show
   end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   resources :articles
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/blog', to: 'articles#index', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
